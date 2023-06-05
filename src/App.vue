f62ce368bcb1668ba107d324c1ee79e976ad4e7e
Darwin gomez <dagomezp@ufpso.edu.co>
2023-06-05 13:20:39 -0500

    eliminar y agregar

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue';
+import CardComponent from './components/CardComponent.vue';
+
+const users = ref([
+  {
+    name: 'Darwin Gomez',
+    mail: 'darwin@gmail.com'
+  },
+  {
+    name: 'Andrey Pacheco',
+    mail: 'pacheco@gmail.com'
+  },
+
+])
+const borrarTarjeta = (index) => {
+  alert('seguro de elimar',index)
+  console.log('seguro de elimar', index)
+  users.value.splice(index,1)
+  console
+}
+
+let name, mail
+const addUser = () => {
+  const newUser = {
+    name: name,
+    mail: mail
+  }
+  users.value.unshift(newUser)
+}
+</script>
+
+<template>
+  <h1 class="text-2x1">
+    HOLA MUNDO
+  </h1>
+  <input type="text" v-model="name"
+  placeholder="Escriba su nombre" class="border rounded"><br><br>
+  <input type="text" v-model="mail" placeholder="email" class="border rounded"><br>
+  <button class="bg-green-600 px-2 py-1 rounded mt-2" @click="addUser">Añadir</button>
+<div class="space-y-2">
+  <CardComponent v-for="(user,index) in users" :key="index" 
+  :name="user.name" :mail="user.mail" :index="index" @borrarTarjeta="borrarTarjeta"/>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,40 @@
+<script setup>
+const mensaje = () => {
+   console.log('Diste click en la foto');
+}
+const props = defineProps(
+    {
+        name:{
+            type: String,
+            default: 'No name'
+        },
+        mail:{
+            type: String,
+            default: 'no email'
+        }
+    }
+)
+const borrarTarjeta = (index) => {
+    console.log('borrar tarjeta', index)
+    alert('borrar tarjeta', index)
+    emit('borrarTarjeta', index)
+}
+const emit = defineEmits(['borrarTarjeta'])
+</script>>
+
+<template>
+    <div class="w-48 rounded shadow overflow-hidden">
+        <div class="inline-block">
+            <img src="https://picsum.photos/40" alt="">
+        </div>
+        <div class="inline-block">
+            <h2 class="text-sm font-semibold">{{props.name}}</h2>
+            <p class="text-xs">{{props.mail}}</p>
+        </div>
+
+        <span class="inline-block ml-4 bg-red-600 text-white rounded leading-0 py-0 px-1 hover:cursor-pointer hover:bg-red-800" @click="borrarTarjeta(props.index)">
+             X
+         </span>
+    </div>
+   
+</template>
